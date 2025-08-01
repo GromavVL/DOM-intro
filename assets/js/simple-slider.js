b48815a180efee00ac41de9e6cb1df7d2d94cc4a
Vlad <vlad086367@gmail.com>
2025-08-01 21:22:02 +0300

    Add markup

---
+++ assets/js/simple-slider.js
@@ -0,0 +1,18 @@
+const slides = [
+    {
+        src: 'https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'landscape1',
+    },
+    {
+        src: 'https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'landscape2',
+    },
+    {
+        src: 'https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'landscape3',
+    },
+    {
+        src: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'landscape4',
+    },
+];
