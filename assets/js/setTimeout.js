8a567e0d9700965a8bbae05a86e609d426255f30
Vlad <vlad086367@gmail.com>
2025-08-28 23:26:20 +0300

    Add simple stopwatch

--- assets/js/setTimeout.js
+++ assets/js/setTimeout.js
@@ -3,34 +3,65 @@
 // setTimeout
 // setInterval
 
-function timeoutAction() {
-    console.log('Hello');
-}
+// function timeoutAction() {
+//     console.log('Hello');
+// }
 
-console.log('Before timeout');
-setTimeout(timeoutAction, 10000);
+// console.log('Before timeout');
+// setTimeout(timeoutAction, 10000);
 
-let i = 0;
+// let i = 0;
 
-function intervalHendler() {
-    if (i === 5) {
-        return clearInterval(setIntervald);
-    }
-    console.log(++i);
-}
+// function intervalHendler() {
+//     if (i === 5) {
+//         return clearInterval(setIntervald);
+//     }
+//     console.log(++i);
+// }
 
-const setIntervald = setInterval(intervalHendler, 1000);
+// const setIntervald = setInterval(intervalHendler, 1000);
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
 // function deleteBtn(e) {
 //     btn.remove();
 // }
 
-function newColorbtn() {
-    btn.style.backgroundColor = 'green';
-}
+// function newColorbtn() {
+//     btn.style.backgroundColor = 'green';
+// }
 
 // setInterval(deleteBtn, 5000);
 
-btn.addEventListener('click', () => {setInterval(newColorbtn, 2000)});
+// btn.addEventListener('click', () => {setInterval(newColorbtn, 2000)});
+
+const timeEl = document.querySelector('.time');
+const [startBtn, stopBtn, resetBtn] = document.querySelectorAll('.btn-container > button');
+
+let time = 0;
+let setIntervald = null;
+
+function startBtnHendler() {
+    function tick() {
+        time++;
+        updateTime(time);
+    }
+    if(!setIntervald) {setIntervald = setInterval(tick, 1)}
+    
+}
+function stopBtnHendler() {
+    clearInterval(setIntervald);
+    setIntervald = null;
+}
+function resetBtnHendler() {
+    time = 0;
+    updateTime(time);
+}
+
+function updateTime(time) {
+    timeEl.textContent = time;
+}
+
+startBtn.onclick = startBtnHendler;
+stopBtn.onclick = stopBtnHendler;
+resetBtn.onclick = resetBtnHendler;
